56ada49e09198f4b91bdf6a197bdaae452ec9e0c
wang790222 <wang790222@gmail.com>
2019-09-19 10:42:37 -0400

    Start RWD

--- src/Home.js
+++ src/Home.js
@@ -11,14 +11,17 @@ const Home = (props) => {
       <div className="header__block">
         <div className="header__block header__block-img_shade">
           <img className="header__block header__block-img_shade-img" src={imageAma} />
-          <div className="header__block       header__block-img_shade-shade">123456789</div>
+          <div className="header__block       header__block-img_shade-shade">&nbsp;</div>
         </div>
         <div className="header__block header__block-tittle">
           <h2 className="header__block header__block-tittle-main">高中職生青少年國族認同大調查</h2>
           <h4 className="header__block header__block-tittle-sub">跟著政治人物最愛的阿嬤，一起來看看台灣高中職生如何思考台灣的未來!</h4>
         </div>
-        <div className="header__block header__block-btn">
-          <a href="#" className="header--btn-main" onClick={props.btnCallback}>聽阿嬤的話</a>     
+        <div 
+          className="header__block header__block-btn"
+          onClick={props.btnCallback}
+        >
+          聽阿嬤的話
         </div>
       </div>
     </div>
