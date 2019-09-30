e5eca6052b12171ced5d9d70bdc1216cb1c4d7e9
wang790222 <wang790222@gmail.com>
2019-09-30 00:37:05 -0400

    Clean Code

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1357aab547b8295b704116b818a77006.js"
+  "/precache-manifest.8afc830ae24974000a97510b71705fb8.js"
 );
 
 self.addEventListener('message', (event) => {
